314f455cf4c4f316d391a2be04580ec5661e24ff
Aman <aman081118@gmail.com>
2025-12-01 00:38:59 +0530

    Fix: Backend route mismatch is fixed

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,29 @@
-import path from "path";
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  // load .env, .env.local, etc.
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react(), tailwindcss()],
+
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  server: {
-    proxy: {
-      "/api": "http://localhost:5000",
+
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
     },
-  },
+  };
 });
